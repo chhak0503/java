10f00dd9fb6024ffb76ab6ddb02dc4f9579bb04a
개발에 반하다 <64509878+chhak0503@users.noreply.github.com>
2024-07-19 12:31:21 +0900

    Delete JavaExam/src/test5 directory

--- JavaExam/src/test5/q08/Commentable.java
+++
@@ -1,8 +0,0 @@
-package test4.q08;
-
-import java.util.List;
-
-public interface Commentable {
-    public void addComment(Comment comment);
-    public List<Comment> getComments();
-}
--- JavaExam/src/test5/q09/Product.java
+++
@@ -1,6 +0,0 @@
-package test4.q09;
-
-public interface Product {
-    String getName();
-    int getPrice();
-}
