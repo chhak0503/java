9a2407bd79a7396fa1d8565e2dca41fb8a301d34
chhak0503 <chhak0503@gmail.com>
2025-01-27 16:06:47 +0900

    CRUDTest

--- CRUDTest/src/user1/AppMain.java
+++ CRUDTest/src/user1/AppMain.java
@@ -1,5 +1,6 @@
 package user1;
 
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -32,17 +33,41 @@ public class AppMain {
 				isRunning = false;
 				break;
 			case 1:
+				// 입력
 				User1 user = inputUser(sc);
 				dao.insertUser1(user);
 				System.out.println("INSERT 완료...");
 				break;
 			case 2:
+				// 조회
+				List<User1> user1List = dao.selectUser1List();
+				for(User1 user1 : user1List) {
+					System.out.println(user1);
+				}				
 				break;
 			case 3:
+				// 검색
+				String uid = findUser(sc);
+				User1 user1 = dao.selectUser1(uid);
+				
+				if(user1 != null) {
+					System.out.println(user1);	
+				}else {
+					System.out.println("해당 아이디를 찾을 수 없습니다.");
+				}
 				break;
 			case 4:
+				// 수정
+				User1 modifiedUser = modifyUser(sc);
+				dao.updateUser1(modifiedUser);
+				System.out.println("해당 사용자를 수정했습니다.");
 				break;
 			case 5:
+				// 삭제
+				String removeUid = removeUser(sc);
+				
+				dao.deleteUser1(removeUid);
+				System.out.println("해당 사용자를 삭제했습니다.");				
 				break;
 			}
 		}
@@ -50,7 +75,6 @@ public class AppMain {
 	} // main end
 	
 	public static User1 inputUser(Scanner sc) {
-		
 		sc.nextLine(); // 입력 버퍼 비우기
 		
 		System.out.print("아이디 입력 : ");
@@ -66,17 +90,45 @@ public class AppMain {
 		int age = sc.nextInt();
 		
 		User1 user = new User1(uid, name, hp, age);
-		//System.out.println(user);
 		return user;
 	}
 	
-	public static void showUsers() {}
+	public static String findUser(Scanner sc) {
+		sc.nextLine();
+		
+		System.out.print("검색 아이디 입력 : ");
+		String uid = sc.nextLine();
+		
+		return uid;
+	}
 	
-	public static void findUser() {}
+	public static User1 modifyUser(Scanner sc) {
+		sc.nextLine(); // 입력 버퍼 비우기
+		
+		System.out.print("수정 아이디 입력 : ");
+		String uid = sc.nextLine();
+		
+		System.out.print("수정 이름 입력 : ");
+		String name = sc.nextLine();
+		
+		System.out.print("수정 휴대폰 입력 : ");
+		String hp = sc.nextLine();
+		
+		System.out.print("수정 나이 입력 : ");
+		int age = sc.nextInt();
+		
+		User1 user = new User1(uid, name, hp, age);
+		return user;
+	}
 	
-	public static void modifyUser() {}
-	
-	public static void removeUser() {}
+	public static String removeUser(Scanner sc) {
+		sc.nextLine(); // 입력 버퍼 비우기
+		
+		System.out.print("삭제 아이디 입력 : ");
+		String uid = sc.nextLine();
+		
+		return uid;
+	}
 	
 	
 	
